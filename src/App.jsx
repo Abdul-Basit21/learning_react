16d4e0a2d831f2a296f0f2fb5f1e428029803cd1
saadahmed2021 <saad@mage4.com>
2023-12-26 19:55:29 +0500

    Footer update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import Navbar from './components/Navbar'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
