e032290e279a489b22a02e49dd76b4e6fea14054
Abdul-Basit21 <abdul.basit@mage4.com>
2023-12-28 18:19:09 +0500

    footer line updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Hero from './Hero'
-import AboutSection from './AboutSection'
+import React from "react";
+import Hero from "./Hero";
+import AboutSection from "./AboutSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <AboutSection />
-      
     </>
-  )
+  );
 }
